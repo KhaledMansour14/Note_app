8c203c595a0daa6307ba4390f4310d3379df0268
KhaledMansour14 <khaledmansoor14@gmail.com>
2024-11-02 14:50:36 +0200

    Change add note loading

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notesapp/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notesapp/widgets/add_note_form.dart';
-import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
   const AddNoteButtomSheet({super.key});
@@ -14,22 +13,23 @@ class AddNoteButtomSheet extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if (state is AddNoteFailure) {
-              print('There Was an Error ${state.errMessage}');
-            }
-            if (state is AddNoteSuccess) {
-              Navigator.pop(context);
-            }
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoading ? true : false,
-              child: const SingleChildScrollView(child: AddNoteForm()),
-            );
-          },
-        ),
-      ),
-    );
+            listener: (context, state) {
+          if (state is AddNoteFailure) {
+            print('There Was an Error ${state.errMessage}');
+          }
+          if (state is AddNoteSuccess) {
+            Navigator.pop(context);
+          }
+        }, builder: (context, state) {
+          
+          return AbsorbPointer( 
+            absorbing: state is AddNoteLoading ? true : false,
+            child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: SingleChildScrollView(child: AddNoteForm()),
+          ),
+          );
+        }),
+      ),);
   }
 }
--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:notesapp/widgets/constants.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.onTap});
-  final void Function() onTap; 
+  const CustomButton({super.key, required this.onTap, this.isLoading = false,});
+  final void Function() onTap;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -15,8 +16,8 @@ class CustomButton extends StatelessWidget {
           color: kPrimayColor,
           borderRadius: BorderRadius.circular(12),
         ),
-        child: const Center(
-            child: Text(
+        child: Center(
+            child: isLoading ? const SizedBox(height: 24, width: 24, child: CircularProgressIndicator(color: Colors.white)) : const Text(
           'Add',
           style: TextStyle(
             color: Colors.black,
